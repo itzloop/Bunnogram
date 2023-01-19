67cd60b1871675e429e4cc7e3b777f33644635df
ys-mn <lilachearted@gmail.com>
2023-01-19 19:29:38 +0330

    Add main menu scene

---
+++ Assets/Scripts/MainMenuGameControl.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuGameControl : MonoBehaviour
+{
+    public Button PlayButton;
+
+    public Button ExitButton;
+
+    public Button AboutButton;
+
+    public Button SettingsButton;
+
+    public void OnPlayButtonClick()
+    {
+        // redirect to levels scene
+    }
+
+    public void OnExitButtonClick()
+    {
+        // show modal
+    }
+
+    public void OnAboutButtonClick()
+    {
+        // redirect to about us scene
+    }
+
+    public void onSettingsButtonClick()
+    {
+        // redirect to settings scene
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
