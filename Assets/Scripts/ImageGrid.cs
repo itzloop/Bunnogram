c21d3aedebcf2c606cfecd1d0f5d655d874cce9c
itzloop <sina.sh.k78@gmail.com>
2023-01-20 21:45:09 +0330

    Adds number indicators

--- Assets/Scripts/ImageGrid.cs
+++ Assets/Scripts/ImageGrid.cs
@@ -8,7 +8,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
-[RequireComponent(typeof(GridLayoutGroup)), RequireComponent(typeof(Image))]
 public class ImageGrid : MonoBehaviour
 {
     [SerializeField] private PixelatedImage pixelatedImage;
@@ -17,16 +16,25 @@ public class ImageGrid : MonoBehaviour
     private GridLayoutGroup _gridLayoutGroup;
     private Image _image;
     private List<Cell> _cells;
-
+    
+    // Main grid
+    [SerializeField] private GameObject mainGrid;
+    
+    // Number indicators
+    [SerializeField] private GameObject row;
+    [SerializeField] private GameObject col;
+    [SerializeField] private NumberIndicator numberIndicator;
+    
+    // Bottom panel's buttons
     [SerializeField] private Button x;
     [SerializeField] private Button o;
     [SerializeField] private Button hint;
-     
+
     private void Awake()
     {
         // init variables
-        _gridLayoutGroup = gameObject.GetComponent<GridLayoutGroup>();
-        _image = gameObject.GetComponent<Image>();
+        _gridLayoutGroup = mainGrid.GetComponent<GridLayoutGroup>();
+        _image = mainGrid.GetComponent<Image>();
         _cells = new List<Cell>();
         
         // set the image to panel
@@ -50,10 +58,33 @@ public class ImageGrid : MonoBehaviour
         {
             for (var j = 0; j < pixelatedImage.bounds.y; j++)
             {
-                var c = Instantiate(cell, gameObject.transform);
+                var c = Instantiate(cell, mainGrid.transform);
                 c.Init(new Vector2Int(j, i), pixelatedImage.backgroundPixels);
                 _cells.Add(c);
             }
+        }
+        
+        // handle row number indicators
+        var rightPadding = 10;
+        var topPadding = 10;
+        
+        row.GetComponent<GridLayoutGroup>().cellSize = new Vector2(wSquare, hSquare + 100);
+        row.GetComponent<GridLayoutGroup>().constraint = GridLayoutGroup.Constraint.FixedRowCount;
+        row.GetComponent<GridLayoutGroup>().constraintCount = 1;
+        row.GetComponent<RectTransform>().anchoredPosition= new Vector2(-rightPadding, (hImage + hSquare + 100) / 2 + topPadding);
+        for (int i = 0; i < pixelatedImage.bounds.x; i++)
+        {
+            var indicator = Instantiate(numberIndicator, row.transform);
+        }
+        
+        // handle col number indicators
+        col.GetComponent<RectTransform>().anchoredPosition = new Vector2(-(wImage + rightPadding), 0);
+        col.GetComponent<GridLayoutGroup>().constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        col.GetComponent<GridLayoutGroup>().constraintCount = 1;
+        col.GetComponent<GridLayoutGroup>().cellSize = new Vector2(wSquare + 100, hSquare);
+        for (int i = 0; i < pixelatedImage.bounds.y; i++)
+        {
+            var indicator = Instantiate(numberIndicator, col.transform);
         }
     }
 
